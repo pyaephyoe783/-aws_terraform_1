47ded162debaabc9fff085ebb6d111ad5c98cb1f
pyaephyoe783 <mrphyoe006@gmail.com>
2026-09-05 13:46:36 +0630

    change thumb

--- oidc.tf
+++ oidc.tf
@@ -1,7 +1,45 @@
+# resource "aws_iam_openid_connect_provider" "github" {
+#     url = "https://token.actions.githubusercontent.com"
+#     client_id_list = ["sts.amazonaws.com"]
+#     thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+# }
+
+# resource "aws_iam_role" "github_actions_role" {
+#   name = "github-actions-terraform-role"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRoleWithWebIdentity"  // AWS Iam Role ကို tempory credential ခဏသုံးမယ့်လို့ပြောတယ့် action
+#         Effect = "Allow"
+#         Principal = {
+#           Federated = aws_iam_openid_connect_provider.github.arn  //Github ကလာတာတွေကိုပဲခွင့်ပြုတယ်
+#         }
+#         Condition = {
+#           StringLike = {
+#             "token.actions.githubusercontent.com:sub" : "repo:pyaephyoe783/-aws_terraform_1:*"
+#           }
+#           StringEquals = {
+#             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
+
+
 resource "aws_iam_openid_connect_provider" "github" {
-    url = "https://token.actions.githubusercontent.com"
-    client_id_list = ["sts.amazonaws.com"]
-    thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+
+  # GitHub Actions ရဲ့ လက်ရှိ OIDC Thumbprints
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a21d2931987e279f0f9810b74100693a1f87",
+    "d89e3bd43d5d909b47a1897730d54102b8001745"
+  ]
 }
 
 resource "aws_iam_role" "github_actions_role" {
@@ -11,10 +49,10 @@ resource "aws_iam_role" "github_actions_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRoleWithWebIdentity"  // AWS Iam Role ကို tempory credential ခဏသုံးမယ့်လို့ပြောတယ့် action
+        Action = "sts:AssumeRoleWithWebIdentity"
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn  //Github ကလာတာတွေကိုပဲခွင့်ပြုတယ်
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
         Condition = {
           StringLike = {
@@ -28,6 +66,9 @@ resource "aws_iam_role" "github_actions_role" {
     ]
   })
 }
+
+
+
 
 resource "aws_iam_role_policy_attachment" "github_actions_admin" {
   role = aws_iam_role.github_actions_role.name
