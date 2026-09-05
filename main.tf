4080a2a470486dad31688c4719d4e95e3df86314
pyaephyoe783 <mrphyoe006@gmail.com>
2026-09-05 13:13:48 +0630

    Ci/Cd Pipe Line Complete

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ terraform {
     }
   }
 
-  # backend "s3" {
-  #   bucket       = "my-tf-state-bucket-ye-yint-2026"
-  #   key          = "global/s3/terraform.tfstate"
-  #   region       = "us-east-1"
-  #   encrypt      = true
-  #   use_lockfile = true
-  # }
+  backend "s3" {
+    bucket       = "my-tf-state-bucket-ye-yint-2026"
+    key          = "global/s3/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
--- remote_state.tf
+++ remote_state.tf
@@ -5,6 +5,10 @@ resource "aws_s3_bucket" "terraform_state" {
   tags = {
     Name = "Terraform-State-Bucket"
   }
+
+  lifecycle {
+    prevent_destroy = true # Destroy ခေါက်ရင် Bucket ပျက်မသွားအောင် ကာကွယ်ပေးပါသည်
+  }
 }
 
 resource "aws_s3_bucket_versioning" "versioning"{
