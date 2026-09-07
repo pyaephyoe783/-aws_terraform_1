e5b2ac810bb49abdfefb558033caa82bbb98b036
pyaephyoe783 <mrphyoe006@gmail.com>
2026-09-07 10:34:00 +0630

    cicd testing again

--- oidc.tf
+++ oidc.tf
@@ -1,38 +1,6 @@
-# resource "aws_iam_openid_connect_provider" "github" {
-#     url = "https://token.actions.githubusercontent.com"
-#     client_id_list = ["sts.amazonaws.com"]
-#     thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-# }
-
-# resource "aws_iam_role" "github_actions_role" {
-#   name = "github-actions-terraform-role"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRoleWithWebIdentity"  // AWS Iam Role ကို tempory credential ခဏသုံးမယ့်လို့ပြောတယ့် action
-#         Effect = "Allow"
-#         Principal = {
-#           Federated = aws_iam_openid_connect_provider.github.arn  //Github ကလာတာတွေကိုပဲခွင့်ပြုတယ်
-#         }
-#         Condition = {
-#           StringLike = {
-#             "token.actions.githubusercontent.com:sub" : "repo:pyaephyoe783/-aws_terraform_1:*"
-#           }
-#           StringEquals = {
-#             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
-#           }
-#         }
-#       }
-#     ]
-#   })
-# }
-
-
 resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
+  url             = "https://token.actions.githubusercontent.com" //github ကလာတာတွေကိုလက်ခံမယ်
+  client_id_list  = ["sts.amazonaws.com"] // amazon ရဲ့ security token service အတွက်လာကြတာ
 
   # GitHub Actions ရဲ့ လက်ရှိ OIDC Thumbprints
   thumbprint_list = [
@@ -40,7 +8,7 @@ resource "aws_iam_openid_connect_provider" "github" {
     "1c58a21d2931987e279f0f9810b74100693a1f87",
     "d89e3bd43d5d909b47a1897730d54102b8001745"
   ]
-}
+}  // OIDC Provider မှတ်ပုံတင်ခြင်း ( github ကလာတယ့် ဧည့်သည်တွေကို ကျွန်တော်တို့လက်ခံမယ်)
 
 resource "aws_iam_role" "github_actions_role" {
   name = "github-actions-terraform-role"
@@ -49,31 +17,31 @@ resource "aws_iam_role" "github_actions_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRoleWithWebIdentity"
+        Action = "sts:AssumeRoleWithWebIdentity" //WebToken နဲ့လာတယ့်သူတွေ
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = aws_iam_openid_connect_provider.github.arn // gituhub ကလာတယ့်သူတွေ
         }
         Condition = {
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : "repo:pyaephyoe783/-aws_terraform_1:*"
+            "token.actions.githubusercontent.com:sub" : "repo:pyaephyoe783/-aws_terraform_1:*"//ဒီ repo ကလာတယ့်သူတွေ
           }
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"//ဒီဧည့်သည်ကဒ်က sts:amazon အတွက်ထုတ်ထားတာလားစစ်ဆေးတာ
           }
         }
       }
     ]
   })
-}
+} // အခန်းသော့ထုတ်ပေးမယ့်စည်းကမ်းချက်သက်မှတ်ခြင်း  ( Trust Policy )
 
 
 
 
 resource "aws_iam_role_policy_attachment" "github_actions_admin" {
   role = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" //အပေါ်က role ကလာတယ့်သူတွေက admin access ရမယ်လို့ပြောထားတာ
+}  // အခန်းသော့ခွင့်ပြုချက် ( Permission Policy )
 
 output "github_actions_role_arn" {
   value = aws_iam_role.github_actions_role.arn
